0aa3efe2639668d14a0a7f778c72ce0f6ecbe93a
toby.weston <toby.weston@db.com>
2014-02-24 15:16:09 +0000

    adding, umm maven. sorry

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get update
 sudo apt-get -y install oracle-java7-installer
 sudo apt-get -y install oracle-java7-set-default
+
+# maven
+sudo apt-get -y install maven
