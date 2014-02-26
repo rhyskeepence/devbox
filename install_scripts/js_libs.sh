c686642d0f5f0de15b35c530d9d74ba73e41725e
toby.weston <toby.weston@db.com>
2014-02-26 12:01:03 +0000

    adding casperjs

---
+++ install_scripts/js_libs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo npm install --global grunt-cli
+sudo npm install --global phantomjs
+
+# casperjs
+echo "export CASPER_HOME=/vagrant/deps/n1k0-casperjs-4f105a9" >> ~/.bashrc
+echo "export PATH=$CASPER_HOME:$PATH" >> ~/.bashrc
--- install_scripts/node_npm.sh
+++ install_scripts/node_npm.sh
@@ -3,7 +3,3 @@
 sudo apt-add-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -y install nodejs
-
-# useful related technologies
-sudo npm install --global grunt-cli
-sudo npm install --global phantomjs
