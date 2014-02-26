acbcf370489c17efbeafbd1abb4025e4cc70a22f
toby.weston <toby.weston@db.com>
2014-02-26 12:15:10 +0000

    adding strange deps

--- install_scripts/js_libs.sh
+++ install_scripts/js_libs.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+sudo apt-get -y install libfontconfig
+
 sudo npm install --global grunt-cli
 sudo npm install --global phantomjs
 
 # casperjs
 echo "export CASPER_HOME=/vagrant/deps/n1k0-casperjs-4f105a9" >> ~/.bashrc
-echo "export PATH=$CASPER_HOME:$PATH" >> ~/.bashrc
+echo 'export PATH=$CASPER_HOME/bin:$PATH' >> ~/.bashrc
